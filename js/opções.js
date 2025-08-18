183553a803c1af2284fc42124b0525fc1e9f627c
CESAR_C# <lianbastiam@gmail.com>
2025-08-18 15:24:17 -0300

    ad

--- js/opções.js
+++ js/opções.js
@@ -3,11 +3,17 @@ var sound_fundo_game = new Audio("assets/musica-fundo.mp3");
 var som = document.querySelector("[name='sound']");
 var somTexto = document.querySelector("#volume-texto");
 
-som.value = parseInt(localStorage.getItem("som"));
-somTexto.innerText = parseInt(localStorage.getItem("som")) || "50";
+var dificuldade = document.querySelector("[name='dificuldade']");
+var dificuldadeTexto = document.querySelector("#dificuldade-texto");
+
+dificuldade.value = parseInt(localStorage.getItem("dificuldade"));
+dificuldade.innerText = localStorage.getItem("dificuldade");
+
+som.value = parseInt(localStorage.getItem("som")) *100;
+somTexto.innerText = parseInt(localStorage.getItem("som"));
 
 sound_fundo_game.volume = parseInt(localStorage.getItem("som"));
-document.addEventListener("click", () =>{
+document.addEventListener("click", () => {
     sound_fundo_game.play();
 
     sound_fundo_game.loop = true;
@@ -16,48 +22,44 @@ document.addEventListener("click", () =>{
 
 som.addEventListener("change", () => {
     switch (som.value) {
-    case 0:
-        somTexto.innerText = "Mute";
-        localStorage.setItem("som", 0);
-        break;
-    case 100:
-        somTexto.innerText = "Ao maximo, tem certeza?";
-        localStorage.setItem("som", 1);
-        break;
-    default:
-        somTexto.innerText = som.value;
-        localStorage.setItem("som", som.value);
-        break;
+        case 0:
+            somTexto.innerText = "Mute";
+            localStorage.setItem("som", 0);
+            break;
+        case 100:
+            somTexto.innerText = "Ao maximo, tem certeza?";
+            localStorage.setItem("som", 1);
+            break;
+        default:
+            somTexto.innerText = som.value;
+            localStorage.setItem("som", som.value);
+            break;
     }
 })
 
-var dificuldade = document.querySelector("[name='dificuldade']");
-var dificuldadeTexto = document.querySelector("#dificuldade-texto");
 
-dificuldade.value = parseInt(localStorage.getItem("dificuldade"));
-dificuldade.innerText = localStorage.getItem("dificuldade");
 
 dificuldade.addEventListener("change", () => {
     switch (dificuldade.value) {
-        case "1":
+        case 1:
             localStorage.setItem("dificuldade", 0.01);
             dificuldadeTexto.innerText = "Fácil";
             break;
-
-        case "2":
+        case 2:
             localStorage.setItem("dificuldade", 0.02);
             dificuldadeTexto.innerText = "Média";
             break;
 
-        case "3":
+        case 3:
             localStorage.setItem("dificuldade", 0.03);
             dificuldadeTexto.innerText = "Difícil";
             break;
 
-        case "4":
+        case 4:
             localStorage.setItem("dificuldade", 0.04);
             dificuldadeTexto.innerText = "Hardcore";
             break;
+
     }
 
 });
