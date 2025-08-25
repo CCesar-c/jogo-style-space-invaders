1635581db45f04b3486ab73232159e80cb0a9c5a
CESAR_C# <lianbastiam@gmail.com>
2025-08-25 14:35:06 -0300

    fixed

---
+++ files/servidor.js
@@ -0,0 +1,35 @@
+const express =  require('express');
+const path = require('path');
+const { createClient  } = require('redis');
+
+const client = createClient({
+    username: 'default',
+    password: 'CyJmyIYQ7rnzyRypNtwEAa2zNl6lI2og',
+    socket: {
+        host: 'redis-12553.c92.us-east-1-3.ec2.redns.redis-cloud.com',
+        port: 12553
+    }
+});
+
+async function conexao() {
+    await client.connect();
+}
+conexao();
+// back - end 
+const app = express();
+app.use(express.text());
+app.use(express.static(path.join(__dirname, '../files')));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/pontos_enviar', async (req, res) => {
+    const valor = parseInt(req.body.mortes);
+    await client.set("user_points", valor);
+})
+
+app.get('/pontos_receber', async (req, res) => {
+    const pontos = await client.get("user_points");
+    res.send(pontos);
+});
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-
+fetch('/pontos_receber')
+.then(response => parseInt(response.text()))
+.then(result => {
+    document.querySelector(".pontos").innerText = result;
+});
 document.addEventListener("click", () =>{
     var musica = document.querySelector("audio");
     musica.loop = true;
