727548cd671507608a5c332940ef015b767405e9
CESAR_C# <lianbastiam@gmail.com>
2025-08-25 17:01:29 -0300

    fixed

--- files/js/game.js
+++ files/js/game.js
@@ -162,10 +162,8 @@ var puedeShot = false;
 // Atualização
 
 Events.on(engine, "beforeUpdate", () => {
-    if (player && player.label != "muerto" && mortes % 20 == 0 && mortes != 0) { 
-        if ( vida_player > 0) {
-            vida_player += 3;
-        }
+    if (player && player.label != "muerto" && mortes % 20 == 0 && mortes != 0 && vida_player > 0) { 
+        vida_player = vida_player + 3;
     }
     if (player && player.label != "muerto") {
         if (sound_fundo_game && sound_fundo_game.paused) {
